aa0fa4ff7d0edf2e6a0883b8ae2f01c06af6fe4d
Mritunjay Gupta <66947124+Mritunjay004@users.noreply.github.com>
2022-10-09 12:54:35 +0530

    Update mergedIntervals.cpp

--- Arrays/mergedIntervals.cpp
+++ Arrays/mergedIntervals.cpp
@@ -1,17 +1,22 @@
-vector<vector<int>> merge(vector<vector<int>> &intervals) {
-  vector<vector<int>> mergedIntervals;
+vector<vector<int>> merge(vector<vector<int>> &intervals)
+{
+    vector<vector<int>> res;
+    sort(begin(intervals), end(intervals));
+    vector<int> temp = intervals[0];
 
-  sort(intervals.begin(), intervals.end());
-  vector<int> tempInterval = intervals[0];
-
-  for (int i = 1; i < intervals.size(); i++) {
-    if (tempInterval[1] >= intervals[i][0])
-      tempInterval[1] = max(intervals[i][1], tempInterval[1]);
-    else {
-      mergedIntervals.push_back(tempInterval);
-      tempInterval = intervals[i];
+    for (int i = 1; i < intervals.size(); i++)
+    {
+        if (temp[1] >= intervals[i][0])
+        {
+            temp[1] = max(intervals[i][1], temp[1]); // yahi saari ek mein aa jaegi
+        }
+        else
+        {
+            // agar nhi hua toh
+            res.push_back(temp); // jo bana hai wo daalo
+            temp = intervals[i]; // jispr nhi bana wo daaldo
+        }
     }
-  }
-  mergedIntervals.push_back(tempInterval);
-  return mergedIntervals;
+    res.push_back(temp);
+    return res;
 }
